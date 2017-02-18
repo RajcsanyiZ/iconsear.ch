31dcbf9626801b2bb13d387b29c9302dd33040fc
Rajcsányi Zoltán <rajcsanyiz@gmail.com>
2017-02-18 22:35:24 +0100

    font-awesome aliases placed into keywords (no symbol duplicity)

--- js/app.js
+++ js/app.js
@@ -1,187 +1,160 @@
-$(document).ready(function() {
-  $('#goup').click(function(e) {
-    e.preventDefault();
-    window.scrollTo(0,0);
-  });
+// visible of symbol packs
+var packVisible=[];
+filterPack();
+refreshList();
 
-  var packVisible=[];
-  filterPack();
-
-  // change source of symbols
-  $(".filter-switch").click(function(e) {
-    $(this).children('i').toggleClass('fa-check-square-o');
-    $(this).children('i').toggleClass('fa-square-o');
-    $(this).toggleClass('badge-info');
-    $(this).toggleClass('badge-danger');
-
-    filterPack();
-    refreshList();
-  });
-
-
-  // number of list result
-  $('#number-of-symbols').append(symbols.length);
-
-  // build list
-  $symbols = $('#symbols');
-
-
-  var filterTag, symbol, tooltip;
-
-  for(var i=0; i < symbols.length; i++) {
-    symbol = symbols[i];
-    if (symbol.pack === 'font-awesome') {
-      filterSocial = inArray(symbol.id, symbols_fontawesome_brand_icons)?' filter-social':'';
-      var $symbol = $('<div class="symbol pack-'+symbol.pack+filterSocial+'" keyword="'+symbol.keyword+'" id="'+i+'">'+symbol.code+'</div>');
-    } else if(symbol.pack === 'material') {
-      filterSocial = inArray(symbol.id, symbols_material_brand_icons)?' filter-social':'';
-      var $symbol = $('<div class="symbol pack-'+symbol.pack+filterSocial+'" keyword="'+symbol.keyword+'" id="'+i+'" '/*+tooltip*/+'>'+symbol.code+'</div>');
-    }
-    $symbols.append($symbol);
-  }
+// change source of symbols
+$(".filter-switch").click(function(e) {
+  $(this).children('i').toggleClass('fa-check-square-o');
+  $(this).children('i').toggleClass('fa-square-o');
+  $(this).toggleClass('badge-info');
+  $(this).toggleClass('badge-danger');
 
   filterPack();
   refreshList();
+});
 
-  // autosize symbols
-  //$('.autofill2').textfill({ });
+// change keyword on input
+$('#search').on('input', refreshList);
 
-  $('#search').on('input', refreshList);
+// filter pack of the symbols
+function filterPack() {
+  packVisible=[];
 
-  // filter pack of the symbols
-  function filterPack() {
-    packVisible=[];
-
-    if ($('#filter-font-awesome').hasClass("badge-info")) {
-      packVisible.push('.pack-font-awesome');
-    }
-    if ($('#filter-material').hasClass("badge-info")) {
-      packVisible.push('.pack-material');
-    }
+  if ($('#filter-font-awesome').hasClass("badge-info")) {
+    packVisible.push('.pack-font-awesome');
   }
+  if ($('#filter-material').hasClass("badge-info")) {
+    packVisible.push('.pack-material');
+  }
+}
 
-  function refreshList() {
-    var keyword = $('#search_field').val().toLowerCase();
-    var number_of_symbols = 0;
-    var filterSocial = $('#filter-social-btn').hasClass('badge-info');
+/**
+ * refresh visible symbols
+ */
+function refreshList() {
+  var keyword = $('#search_field').val().toLowerCase();
+  var number_of_symbols = 0;
+  var filterSocial = $('#filter-social-btn').hasClass('badge-info');
 
-    $('.symbol').each(function(index) {
-      var $icon = $(this);
-      if ($(this).attr('keyword').search(keyword) === -1) {
-        $icon.hide();
-      } else {
-        if ($(this).is(packVisible.join())) {
-          // filter social symbols
-          if( filterSocial || $icon.hasClass('filter-social') === false) {
-            $icon.show();
-            number_of_symbols++;
-          } else  {
-            $icon.hide();
-          }
-        } else {
+  $('.symbol').each(function(index) {
+    var $icon = $(this);
+    if ($(this).attr('keyword').search(keyword) === -1) {
+      $icon.hide();
+    } else {
+      if ($(this).is(packVisible.join())) {
+        // filter social symbols
+        if( filterSocial || $icon.hasClass('filter-social') === false) {
+          $icon.show();
+          number_of_symbols++;
+        } else  {
           $icon.hide();
         }
+      } else {
+        $icon.hide();
       }
-    });
-    $('#number-of-symbols').text(number_of_symbols);
-  }
-
-  // change size of symbols
-  $('#ico-size-small').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-lg').addClass('symbol-sm');
-  });
-
-  $('#ico-size-default').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-lg symbol-sm');
-  });
-
-  $('#ico-size-large').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-sm').addClass('symbol-lg');
-  });
-
-  //  change theme of symbols
-  // change size of symbols
-  $('#ico-theme-1').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-theme-2').addClass('symbol-theme-1');
-    $('#wrap').removeClass('symbol-theme-2-bg').addClass('symbol-theme-1-bg');
-    $('h1.category').removeClass('symbol-theme-2-h1').addClass('symbol-theme-1-h1');
-    $('#wrap-symbols').addClass('wrap-symbols-theme');
-  });
-
-  $('#ico-theme-2').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-theme-1').addClass('symbol-theme-2');
-    $('#wrap').removeClass('symbol-theme-1-bg').addClass('symbol-theme-2-bg');
-    $('h1.category').removeClass('symbol-theme-1-h1').addClass('symbol-theme-2-h1');
-    $('#wrap-symbols').addClass('wrap-symbols-theme');
-  });
-
-  $('#ico-theme-default').click(function(e) {
-    e.preventDefault();
-    $('.symbol').removeClass('symbol-theme-1 symbol-theme-2');
-    $('#wrap').removeClass('symbol-theme-1-bg symbol-theme-2-bg');
-    $('h1.category').removeClass('symbol-theme-1-h1 symbol-theme-2-h1');
-    $('#wrap-symbols').removeClass('wrap-symbols-theme');
-  });
-
-  // open showSymbol modal window
-  $('.symbol').click(function(e) {
-    e.preventDefault();
-
-    // load data of symbol
-    var symbol = symbols[$(this).attr('id')]
-
-    // refresh modal from data
-    $('#symbol-html').html(symbol.code);
-
-    $('#symbol-pack-link').attr('href', symbol.packLink);
-    $('#symbol-pack-name').html(symbol.pack);
-
-    $('#symbol-link').attr('href', symbol.link);
-    $('#symbol-name').html(symbol.name);
-
-    $('#symbol-code').text(symbol.code);
-    $('#symbol-unicode').text(symbol.unicode);
-
-    $('#symbol-keyword').text(symbol.keyword);
-
-    // warning message show only brand icons
-    $('#alert-font-awesome').hide();
-    $('#alert-material').hide();
-    if (symbol.pack === 'font-awesome' && inArray(symbol.id, symbols_fontawesome_brand_icons)) {
-        $('#alert-font-awesome').show();
-    } else if(symbol.pack === 'material' && inArray(symbol.id, symbols_material_brand_icons)) {
-        $('#alert-material').show();
     }
-
-    $('#infoSymbol').modal();
   });
+  $('#number-of-symbols').text(number_of_symbols);
+}
 
-  function inArray(needle, haystack) {
-    var length = haystack.length;
-    for(var i = 0; i < length; i++) {
-        if(haystack[i] == needle) return true;
-    }
-    return false;
-  }
-
-  $('[data-toggle="tooltip"]').tooltip()
-
-  // open terms.html to a modal window
-  $.get('terms.html', function(data) {
-    $('#terms-content').html($('#wrap-terms-inner', data));
-  });
-
-  // open terms modal
-  $('#btn-terms').click(function(e) {
-    e.preventDefault();
-    $('#terms').modal();
-  });
-
-
- new Clipboard('.btn');
+// change size of symbols to small
+$('#ico-size-small').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-lg').addClass('symbol-sm');
 });
+
+// change size of symbols to medium
+$('#ico-size-default').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-lg symbol-sm');
+});
+
+// change size of symbols to large
+$('#ico-size-large').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-sm').addClass('symbol-lg');
+});
+
+//  change theme of symbols to light contrast
+$('#ico-theme-1').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-theme-2').addClass('symbol-theme-1');
+  $('#wrap').removeClass('symbol-theme-2-bg').addClass('symbol-theme-1-bg');
+  $('h1.category').removeClass('symbol-theme-2-h1').addClass('symbol-theme-1-h1');
+  $('#wrap-symbols').addClass('wrap-symbols-theme');
+});
+
+//  change theme of symbols to light
+$('#ico-theme-2').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-theme-1').addClass('symbol-theme-2');
+  $('#wrap').removeClass('symbol-theme-1-bg').addClass('symbol-theme-2-bg');
+  $('h1.category').removeClass('symbol-theme-1-h1').addClass('symbol-theme-2-h1');
+  $('#wrap-symbols').addClass('wrap-symbols-theme');
+});
+
+//  change theme of symbols to default dark
+$('#ico-theme-default').click(function(e) {
+  e.preventDefault();
+  $('.symbol').removeClass('symbol-theme-1 symbol-theme-2');
+  $('#wrap').removeClass('symbol-theme-1-bg symbol-theme-2-bg');
+  $('h1.category').removeClass('symbol-theme-1-h1 symbol-theme-2-h1');
+  $('#wrap-symbols').removeClass('wrap-symbols-theme');
+});
+
+// open showSymbol modal window
+$('.symbol').click(function(e) {
+  e.preventDefault();
+
+  // load data of symbol
+  var symbol = symbols[$(this).attr('id')]
+
+  // refresh modal from data
+  $('#symbol-html').html(symbol.code);
+
+  // top of the window: symbol pack name and link and symbol name and link
+  $('#symbol-pack-link').attr('href', symbol.packLink);
+  $('#symbol-pack-name').html(symbol.pack);
+  $('#symbol-link').attr('href', symbol.link);
+  $('#symbol-name').html(symbol.name);
+
+  // source and unicode text to use symbol
+  $('#symbol-code').text(symbol.code);
+  $('#symbol-unicode').text(symbol.unicode);
+
+  // keywords to the symbol
+  $('#symbol-keyword').text(symbol.keyword);
+
+  // warning message show only brand icons
+  $('#alert-font-awesome').hide();
+  $('#alert-material').hide();
+
+  // brand icon show extra legal infos
+  if (symbol.pack === 'font-awesome' && inArray(symbol.id, symbols_fontawesome_brand_icons)) {
+      $('#alert-font-awesome').show();
+  } else if(symbol.pack === 'material' && inArray(symbol.id, symbols_material_brand_icons)) {
+      $('#alert-material').show();
+  }
+
+  // open modal window
+  $('#infoSymbol').modal();
+});
+
+// using bootstrap tooltip
+$('[data-toggle="tooltip"]').tooltip()
+
+// open terms.html to a modal window
+$.get('terms.html', function(data) {
+  $('#terms-content').html($('#wrap-terms-inner', data));
+});
+
+// open terms modal window
+$('#btn-terms').click(function(e) {
+  e.preventDefault();
+  $('#terms').modal();
+});
+
+
+// use clipboard jquery plugin to copy source an unicode info of symbol
+new Clipboard('.btn');
---
+++ js/bootstrap.js
@@ -0,0 +1,73 @@
+// generate symbols array
+var symbols = [];
+
+// import font-awesome symbols
+var flagAlias = -1;
+for (i=0; i<symbols_fontawesome.length; i++) {
+	var symbol = symbols_fontawesome[i];
+
+	// has it an alias?
+	for (var j=0; j < symbols.length; j++) {
+		if (symbols[j].unicode === symbol.unicode) {
+			flagAlias = j;
+			break;
+		}
+	}
+	// same icons group with alias
+	if (flagAlias === -1) {
+		// it is a new symbol
+		symbol['name'] = symbol.id;
+		symbol['pack'] = 'font-awesome';
+		symbol['code'] = '<i class="fa fa-'+symbol.id+'"></i>';
+		symbol['packLink'] = 'http://fontawesome.io';
+		symbol['link'] = 'http://fontawesome.io/icon/'+symbol.id;
+		symbols.push(symbol);
+	} else {
+		// it is an alias
+		symbols[flagAlias].keyword += ', '+ symbol.id;
+		flagAlias = -1;
+
+	}
+}
+
+// import material icons
+for (i=0; i < symbols_material.length; i++) {
+	var symbol = symbols_material[i];
+	symbol['pack'] = 'material';
+	symbol['code'] = '<i class="zmdi zmdi-'+symbol.id+'"></i>';
+	symbol['packLink'] = 'http://zavoloklom.github.io/material-design-iconic-font/icons.html';
+	symbol['link'] = 'http://zavoloklom.github.io/material-design-iconic-font/icons.html#'+symbol.id;
+	symbols.push(symbol);
+}
+
+
+// load symbols into DOM
+$symbols = $('#symbols');
+
+var filterTag, symbol, tooltip;
+
+for(var i=0; i < symbols.length; i++) {
+  symbol = symbols[i];
+  if (symbol.pack === 'font-awesome') {
+    filterSocial = inArray(symbol.id, symbols_fontawesome_brand_icons)?' filter-social':'';
+    var $symbol = $('<div class="symbol pack-'+symbol.pack+filterSocial+'" keyword="'+symbol.keyword+'" id="'+i+'">'+symbol.code+'</div>');
+  } else if(symbol.pack === 'material') {
+    filterSocial = inArray(symbol.id, symbols_material_brand_icons)?' filter-social':'';
+    var $symbol = $('<div class="symbol pack-'+symbol.pack+filterSocial+'" keyword="'+symbol.keyword+'" id="'+i+'" '/*+tooltip*/+'>'+symbol.code+'</div>');
+  }
+  $symbols.append($symbol);
+}
+
+/**
+ * has a item on array
+ * @param  mixed needle
+ * @param array haystack
+ * @return boolean
+ */
+function inArray(needle, haystack) {
+  var length = haystack.length;
+  for(var i = 0; i < length; i++) {
+      if(haystack[i] == needle) return true;
+  }
+  return false;
+}
